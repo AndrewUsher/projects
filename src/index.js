39b335855ca32a0382005641b1901d31098d1d4f
Andrew Usher <andrewusher00@gmail.com>
2019-10-31 12:39:42 -0500

    refactor(App): upgrade to experimental react w/ concurrent mode

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { render } from 'react-dom'
+import ReactDOM from 'react-dom'
 import App from './containers/App'
 
-render(<App />, document.getElementById('root'))
+ReactDOM.createRoot(document.getElementById('root')).render(<App />)
